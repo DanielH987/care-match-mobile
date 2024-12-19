8d03dec936517d35af18d041af7e302c1abb854b
Daniel Hootini <hootinid@gmail.com>
2024-12-18 16:32:20 -1000

    add authentication redirection in layout component

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { Slot } from 'expo-router';
+import { View } from 'react-native';
+import { Slot, useSegments } from 'expo-router';
+import { AuthContextProvider, useAuth } from '../context/authContext';
+import React, { useEffect } from 'react';
 import "../global.css";
 
-export default function _layout() {
+const MainLayout = () => {
+  const [isAuthenticated] = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to login if not authenticated
+    if (typeof isAuthenticated == 'undefined') return;
+    const inApp = segments[0] == '(app)';
+    if (!isAuthenticated && !inApp) {
+      // Redirect to Home
+      router.replace('home');
+    } else if (isAuthenticated == false) {
+      // Redirect to signIn
+      router.replace('signIn');
+    }
+  }, [isAuthenticated]);
+
+  return <Slot />;
+}
+
+export default function RootLayout() {
   return (
-    <View className="flex-1">
-        <Slot />
-    </View>
+    <AuthContextProvider>
+        <MainLayout />
+    </AuthContextProvider>
   );
 }
