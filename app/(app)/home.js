7d7b2a283f1ea302d48a71865a87c9987b35bf61
Daniel Hootini <hootinid@gmail.com>
2024-12-20 13:14:25 -1000

    add logout functionality to Home component with navigation to index

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React from 'react';
+import { useAuth } from '../../context/authContext';
+import { Button } from 'react-native-web';
+import { useRouter } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
+
+  const {logout} = useAuth();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    console.log('logout');
+    await logout();
+    router.push('index');
+  };
+
   return (
     <View>
         <Text>Home</Text>
+        <Pressable onPress={handleLogout}>
+            <Text>Sign Out</Text>
+        </Pressable>
     </View>
   );
 }
