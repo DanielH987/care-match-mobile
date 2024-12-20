eedc8b0ba899e34f017d28331a9c84a22d4d222f
Daniel Hootini <hootinid@gmail.com>
2024-12-20 13:38:39 -1000

    remove unused imports and streamline logout functionality in Home component

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,19 +1,13 @@
 import { View, Text, Pressable } from 'react-native';
 import React from 'react';
 import { useAuth } from '../../context/authContext';
-import { Button } from 'react-native-web';
-import { useRouter } from 'expo-router';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
 
   const {logout} = useAuth();
-  const router = useRouter();
 
   const handleLogout = async () => {
-    console.log('logout');
     await logout();
-    router.push('index');
   };
 
   return (
