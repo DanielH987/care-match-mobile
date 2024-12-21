e48b6ec645c60e969a0d0504b453413639178deb
Daniel Hootini <hootinid@gmail.com>
2024-12-20 16:48:33 -1000

    refactor layout component to use SafeAreaView and StatusBar; add HomeHeader for improved navigation

--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import React from 'react';
 import { Stack } from 'expo-router';
+import HomeHeader from '../../components/HomeHeader';
 
 export default function _layout() {
-  return (
-    <Stack/>
-  );
+    return (
+        <SafeAreaView className="flex-1 bg-indigo-400">
+            <StatusBar className="bg-indigo-400"/>
+            <Stack>
+                <Stack.Screen
+                    name="home"
+                    options={{
+                        header: () => <HomeHeader />,
+                    }}
+                />
+            </Stack>
+        </SafeAreaView>
+    );
 }
