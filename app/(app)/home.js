2fd9e83604f9d092674a630ac9b14dfc4108f084
Daniel Hootini <hootinid@gmail.com>
2024-12-26 09:57:07 -1000

    feat: add user fetching logic and loading indicator to Home component

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,17 +1,22 @@
-import { View, Text } from 'react-native';
-import React, { useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/authContext';
 import { StatusBar } from 'expo-status-bar';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { ActivityIndicator } from 'react-native-web';
+import ChatList from '../../components/ChatList';
+import Loading from '../../components/Loading';
+import { use } from 'react';
 
 export default function Home() {
 
-  const {logout} = useAuth();
+  const {logout, user} = useAuth();
   const [users, setUsers] = useState([1,2,3]);
 
-  const handleLogout = async () => {
-    await logout();
+  useEffect(() => {
+    if (user?.uid) getUsers();
+  }, []);
+
+  const getUsers = async () => {
   };
 
   return (
@@ -23,7 +28,8 @@ export default function Home() {
             <ChatList users={users} />
           ) : (
             <View className="flex items-center" style={{top: hp(30)}}>
-              <ActivityIndicator size="larger" />
+              <ActivityIndicator size="large" />
+              {/* <Loading size={hp(30)}/> */}
             </View>
           )
         }
