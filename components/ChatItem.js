6fa39595f0fa4bb5524d294a7e18c91c55bebe24
Daniel Hootini <hootinid@gmail.com>
2024-12-26 10:09:42 -1000

    feat: enhance ChatItem component with user details and conditional border

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -2,13 +2,36 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-export default function ChatItem({item, index}) {
+export default function ChatItem({item, router, noBorder}) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className={`flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 ${noBorder ? '' : 'border-b border-neutral-200'}`}>
         <Image 
             source={require('../assets/images/react-logo.png')} 
-            style={{ height: hp(6), aspectRatio: 1 }} 
+            style={{ height: hp(6), width: hp(6) }}
+            className='rounded-full'
         />
+
+        {/* Name and last messages */}
+        <View className='flex-1 gap-1'>
+            {/* Name */}
+            <View className='flex-row justify-between'>
+                <Text
+                    style={{ fontSize: hp(1.8) }}
+                    className='font-semibold text-neutral-800'
+                >
+                    Daniel
+                </Text>
+                <Text
+                    style={{ fontSize: hp(1.6) }}
+                    className='font-medium text-neutral-500'
+                >
+                    Time
+                </Text>
+            </View>
+
+            {/* Last message */}
+            <Text style={{ fontSize: hp(1.6) }} className='font-medium text-neutral-500'>Last message</Text>
+        </View>
     </TouchableOpacity>
   );
 }
