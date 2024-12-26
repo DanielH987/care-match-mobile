4ead3b966ae7eaa261c6dde69edd2b1ac1eb5e62
Daniel Hootini <hootinid@gmail.com>
2024-12-26 11:41:11 -1000

    feat: enhance CustomKeyboardView to support chat-specific configurations

--- components/CustomkeyboardView.js
+++ components/CustomkeyboardView.js
@@ -2,19 +2,28 @@ import { View, Text, KeyboardAvoidingView, ScrollView, Platform } from 'react-na
 import React from 'react';
 
 const ios = Platform.OS === 'ios';
-export default function CustomKeyboardView({children}) {
+export default function CustomKeyboardView({ children, inChat }) {
+  let kavConfig = {};
+  let scrollViewConfig = {};
+  if (inChat) {
+    kavConfig = {keyboardVerticalOffset: 90};
+    scrollViewConfig = {contentContainerStyle: {flex: 1}};
+  }
+
   return (
     <KeyboardAvoidingView
-        behavior={ios ? 'padding' : 'height'}
-        style={{ flex: 1 }}
+      behavior={ios ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+      {...kavConfig}
     >
-        <ScrollView
-            style={{ flex: 1 }}
-            bounces={false}
-            showsVerticalScrollIndicator={false}
-        >
-            {children}
-        </ScrollView>
+      <ScrollView
+        style={{ flex: 1 }}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        {...scrollViewConfig}
+      >
+        {children}
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
